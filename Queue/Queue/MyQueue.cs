c1d9ea90e118cb9a2487756a63c212c09738af66
kiselov <kroliktin@gmail.com>
2016-06-08 11:40:10 +0300

    Add files via upload

---
+++ Queue/Queue/MyQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class MyQueue
+    {
+        private Stack<int> stack = new Stack<int>();
+        private Stack<int> temp = new Stack<int>();
+
+        public void Push(int Value)
+        {
+            stack.Push(Value);
+        }
+
+        public int Pop()
+        {
+            while (stack.Count != 0)
+                temp.Push(stack.Pop());
+
+            int Value = temp.Pop();
+
+            while (temp.Count != 0)
+                stack.Push(temp.Pop());
+
+            return Value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            while (stack.Count != 0)
+                temp.Push(stack.Pop());
+
+            while (temp.Count != 0)
+            {
+                sb.Append(temp.Peek());
+                sb.Append(" ");
+                stack.Push(temp.Pop());
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue q = new MyQueue();
+            q.Push(1);
+            q.Push(2);
+            q.Push(3);
+
+            Console.WriteLine("{0,-10} - исходная очередь", q);
+            Console.WriteLine("{1,-10} - После удаления {0}", q.Pop(), q);
+            Console.WriteLine("{1,-10} - После удаления {0}", q.Pop(), q);
+
+            Console.ReadLine();
+        }
+    }
+}
